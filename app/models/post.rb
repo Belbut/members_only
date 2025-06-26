f8ba35ac05dc54b5b2fb24df6b1406a1b940e73f
Belbut <80793911+Belbut@users.noreply.github.com>
2025-06-26 14:36:46 +0100

    feat: add validation to the user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  validates :comment, presence: true, length: { in: 1..250 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true, presence: true, length: { in: 3..20 }
+  validates :email, uniqueness: true, presence: true
+  validates :password, length: { in: Devise.password_length }
 end
