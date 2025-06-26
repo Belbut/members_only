8ff506aed695aea67bc6879c9bf0dd7732f4dd57
Belbut <80793911+Belbut@users.noreply.github.com>
2025-06-26 16:01:40 +0100

    fix: change routes to RESTful standard
    I used "rails generate controller Posts new create index", thinking it
    would handle the creation of the routes for the three actions.
    
    But the reality is that the generator couldn't have known that the
    actions that i specified where in fact the standard RESTful actions, so
    it created them has 3 simple GET actions
    
    note: don't use rails generate controller for the actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
-
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -16,4 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'posts#index'
+
+  resources :posts, only: %i[index new create]
 end
