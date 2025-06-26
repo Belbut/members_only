eeecc66f35588bde833b0d12a645f45c078b42f9
Belbut <80793911+Belbut@users.noreply.github.com>
2025-06-26 15:38:09 +0100

    feat: complete Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,27 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      flash.now[:error] = 'Could not save client'
+      puts post_params
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def index
+  private
+
+  def post_params
+    params.expect(post: [:comment])
   end
 end
